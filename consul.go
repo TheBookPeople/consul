c6c8c51a4f0e4de58c0cd47ef00beb61f98f45cd
William Griffiths <william.griffiths@thebookpeople.co.uk>
2017-02-23 17:18:51 +0000

    Initial commit

---
+++ consul.go
@@ -0,0 +1,38 @@
+package consul
+
+import (
+	"fmt"
+	"math/rand"
+	"net"
+	"strings"
+)
+
+//Lookup - Given a consul DNS name it returns the host and port of an random instance
+//of that service. Consul DNS names must end in .consul
+func Lookup(service string) (*string, error) {
+	return lookupWihLookupSRV(service, net.LookupSRV)
+}
+
+type lookupSRV func(service, proto, name string) (cname string, addrs []*net.SRV, err error)
+
+func lookupWihLookupSRV(service string, lookup lookupSRV) (*string, error) {
+	if !strings.HasSuffix(service, ".consul") {
+		return nil, fmt.Errorf("%q is not a consul URL. Needs to end with .consul", service)
+	}
+
+	_, srvs, err := lookup("", "", service)
+
+	if err != nil {
+		return nil, fmt.Errorf("Error performing SRV DNS Lookup for %s - %s", service, err.Error())
+	}
+
+	if srvs == nil || len(srvs) == 0 {
+		return nil, nil
+	}
+
+	srv := srvs[rand.Intn(len(srvs))]
+	target := fmt.Sprintf("%s:%d", strings.TrimSuffix(srv.Target, "."), srv.Port)
+
+	return &target, nil
+
+}
---
+++ consul_test.go
@@ -0,0 +1,99 @@
+package consul
+
+import (
+	"errors"
+	"fmt"
+	"net"
+	"reflect"
+	"testing"
+)
+
+func ExampleLookup() {
+	service := "authentication.service.consul"
+	r, err := Lookup(service)
+
+	if err != nil {
+		fmt.Printf("Error Looking up consul service %q - %s\n", service, err.Error())
+	}
+
+	//services-dev-03.node.dc1.consul:8081
+	fmt.Println(*r)
+}
+
+func TestLookup(t *testing.T) {
+	dummyLookup := func(service, proto, name string) (cname string, addrs []*net.SRV, err error) {
+		result := net.SRV{
+			Target: "node1.example.com.consul.",
+			Port:   8089,
+		}
+		results := []*net.SRV{&result}
+		return "", results, nil
+	}
+
+	service := "example.com.consul"
+	r, err := lookupWihLookupSRV(service, dummyLookup)
+	assertNil(t, err, "Error should have been nil")
+	assertEqual(t, "node1.example.com.consul:8089", *r)
+}
+
+func TestLookup_InvalidName(t *testing.T) {
+	service := "example.com"
+	r, err := Lookup(service)
+	assertEqual(t, `"example.com" is not a consul URL. Needs to end with .consul`, err.Error())
+	assertNil(t, r, "Result should have been nil")
+}
+
+func TestLookup_LookupError(t *testing.T) {
+	errorLookup := func(service, proto, name string) (cname string, addrs []*net.SRV, err error) {
+		return "", nil, errors.New("Boom")
+	}
+
+	service := "example.com.consul"
+	r, err := lookupWihLookupSRV(service, errorLookup)
+
+	assertEqual(t, "Error performing SRV DNS Lookup for example.com.consul - Boom", err.Error())
+	assertNil(t, r, "Result should have been nil")
+}
+
+func TestLookup_LookupNilResults(t *testing.T) {
+	errorLookup := func(service, proto, name string) (cname string, addrs []*net.SRV, err error) {
+		return "", nil, nil
+	}
+
+	service := "example.com.consul"
+	r, err := lookupWihLookupSRV(service, errorLookup)
+	assertNil(t, err, "Error should have been nil")
+	assertNil(t, r, "Result should have been nil")
+}
+
+func assertNil(t *testing.T, i interface{}, message string) {
+	if !isNil(i) {
+		t.Fatal(message)
+	}
+}
+
+func assertNotNil(t *testing.T, i interface{}, message string) {
+	if isNil(i) {
+		t.Fatal(message)
+	}
+}
+
+func isNil(object interface{}) bool {
+	if object == nil {
+		return true
+	}
+
+	value := reflect.ValueOf(object)
+	kind := value.Kind()
+	if kind >= reflect.Chan && kind <= reflect.Slice && value.IsNil() {
+		return true
+	}
+
+	return false
+}
+
+func assertEqual(t *testing.T, expected string, result string) {
+	if result != expected {
+		t.Fatalf("Expected %q gor %q", expected, result)
+	}
+}
